3ac9bfa06f730f96adb93b52ae0282305f426ea2
theUniversityFinder <ganjisai19@gmail.com>
2023-12-29 21:29:50 +0530

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config() // load env vars from .env
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    __VALUE__: `"${process.env.VALUE}"`, // wrapping in "" since it's a string
+  },
+  server: {
+    proxy: {
+      "/api": "https://torrent-searcher-api-with-website.bharathganji.repl.co/",
+    }  
+  } ,
+});
